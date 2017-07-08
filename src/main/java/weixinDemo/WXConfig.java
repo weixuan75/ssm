573d8835e405559cf33816ce620b61bb6a971bec
weixuan <weixuan75@163.com>
2017-07-08 14:38:21 +0800

    初始化

---
+++ src/main/java/weixinDemo/WXConfig.java
@@ -0,0 +1,11 @@
+package weixinDemo;
+
+public class WXConfig {
+	//	appid	是	第三方用户唯一凭证
+	private String APPID;
+	//	secret	是	第三方用户唯一凭证密钥，即appsecret
+	private String APPSECRET;
+	//	grant_type	是	获取access_token填写client_credential
+	private String GRANTTYOPE = "client_credential";
+
+}
